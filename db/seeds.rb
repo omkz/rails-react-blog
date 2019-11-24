78889ab2d83e39d877a7cb03ffe57d5b87902c24
Kurnia Muhamad <omenkzz@gmail.com>
2019-11-24 11:52:44 +0700

    Add seed db for post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+posts = Post.create([
+                      { title: 'game of thrones',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' },
+                      { title: 'bourne identity',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' },
+                      { title: 'jack reacher',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' },
+                      { title: 'john wick 2',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' }
+                    ])
