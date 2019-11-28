e02808c31885f5806939b0c566d5431b3b0716e2
Kurnia Muhamad <omenkzz@gmail.com>
2019-11-28 15:17:31 +0700

    Update navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import AllPosts from "./components/AllPosts";
 import About from './components/About';
 import Contact from './components/Contact';
-import { Menu } from 'semantic-ui-react'
+import { Menu, Container} from 'semantic-ui-react'
 
 
 function App() {
   return (
     <Router>
-      <Menu inverted>
-        <Menu.Item as={Link} to="/" > Home</Menu.Item>
-        <Menu.Item as={Link} to="/contact"> Contact</Menu.Item>
-        <Menu.Item as={Link} to="/about"> About</Menu.Item>
+      <Menu fixed='top' inverted>
+        <Container>
+          <Menu.Item as={Link} to="/" > Home</Menu.Item>
+          <Menu.Item as={Link} to="/contact"> Contact</Menu.Item>
+          <Menu.Item as={Link} to="/about"> About</Menu.Item>
+        </Container>
       </Menu>
+      
       <Switch>
         <Route exact path='/' component={AllPosts} />
         <Route path='/contact' component={Contact} />
