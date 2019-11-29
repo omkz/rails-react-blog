efc5624682c78f2fb8524d0165b7fcdca7228987
Kurnia Muhamad <omenkzz@gmail.com>
2019-11-29 17:30:17 +0700

    Remove contact and about components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import About from './components/About';
-import Contact from './components/Contact';
 import { Menu } from 'semantic-ui-react'
 import { HomePage } from './HomePage';
 import CreatePost from './components/CreatePost';
@@ -14,8 +12,6 @@ function App() {
       <Menu fixed='top' inverted>
         <Menu.Item as={Link} to="/"> Home</Menu.Item>
         <Menu.Item as={Link} to="/createpost">Create Post</Menu.Item>
-        <Menu.Item as={Link} to="/contact"> Contact</Menu.Item>
-        <Menu.Item as={Link} to="/about"> About</Menu.Item>
         <Menu.Menu position='right'>
           <Menu.Item as={Link} to="/login">Login</Menu.Item>
         </Menu.Menu>
@@ -24,8 +20,6 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/createpost' component={CreatePost} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/about' component={About} />
         <Route path='/login' component={LoginPage} />
       </Switch>
 
