273d4e465b497f3ce39258c796cdf1dec2de07ba
Kurnia Muhamad <omenkzz@gmail.com>
2019-11-30 15:00:38 +0700

    Add post deletion

--- client/src/components/DetailPost.jsx
+++ client/src/components/DetailPost.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { Container, Header, Segment } from "semantic-ui-react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class DetailPost extends React.Component {
   constructor(props) {
@@ -30,6 +30,19 @@ class DetailPost extends React.Component {
       });
   }
 
+  deletePost(id) {
+    if (window.confirm("Are you sure?")) {
+      axios
+        .delete(`/api/v1/posts/${id}`)
+        .then(res => {
+          toast.error("Post successfully deleted");
+          this.props.history.push("/");
+        }).catch(function(error) {
+          toast.error(JSON.stringify(error.response.data));
+        });
+    }
+  }
+
   render() {
     return (
       <Segment style={{ padding: "8em 0em" }} vertical>
@@ -38,7 +51,19 @@ class DetailPost extends React.Component {
             {this.state.title}
           </Header>
           <p style={{ fontSize: "1.33em" }}>{this.state.content}</p>
-          <Link to={"/posts/" + this.props.match.params.id + "/edit/"} className="ui button">Edit</Link>
+          <Link
+            to={"/posts/" + this.props.match.params.id + "/edit/"}
+            className="ui button"
+          >
+            Edit
+          </Link>
+          <Link
+            to={"#"}
+            onClick={() => this.deletePost(this.props.match.params.id)}
+            className="ui button"
+          >
+            Delete
+          </Link>
         </Container>
       </Segment>
     );
