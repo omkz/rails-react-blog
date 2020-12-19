22d96ec3342ad4aa452ab2e4106f596605eeddf9
Kurnia Muhamad <omenkzz@gmail.com>
2020-12-19 22:04:55 +0700

    Generate data using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,5 @@
-posts = Post.create([
-                      { title: 'game of thrones',
-                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' },
-                      { title: 'bourne identity',
-                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' },
-                      { title: 'jack reacher',
-                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' },
-                      { title: 'john wick 2',
-                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                        content: 'Ultricies tristique nulla aliquet enim. Vivamus arcu felis bibendum ut. Id nibh tortor id aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et malesuada fames ac turpis egestas maecenas pharetra. ' }
-                    ])
+100.times do
+  Post.create(title: Faker::Movie.title,
+              description: Faker::Movie.quote,
+              content: Faker::Lorem.paragraphs)
+end
